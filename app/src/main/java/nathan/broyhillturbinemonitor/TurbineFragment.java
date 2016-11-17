592e993022b9a4f5200a5c4d2ef6074b8de84f6a
Nathan Davis <davisna1@appstate.edu>
2016-11-17 10:19:06 -0500

    Initial commit

---
+++ app/src/main/java/nathan/broyhillturbinemonitor/TurbineFragment.java
@@ -0,0 +1,15 @@
+package nathan.broyhillturbinemonitor;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Owner on 11/17/2016.
+ */
+
+public class TurbineFragment extends Fragment{
+
+    public static TurbineFragment newInstance() {
+        return new TurbineFragment();
+    }
+
+}
